57f9a90120877b9b7f716c9203c875523475c6f7
melicard <bryannm92@gmail.com>
2018-03-21 10:53:09 +0000

    Add files via upload

---
+++ doubleclicktogo.js
@@ -0,0 +1,27 @@
+﻿(function ($) {
+    $.fn.doubleClickToGo = function () {
+        var secondForDoubleClick = .5; //Add more seconds to increase the interval when two click are considered double click
+        var firstClickTime = null;
+        var secondClickTime = null;
+        this.filter("a").each(function () {
+            $(this).click(function () {
+                if (firstClickTime == null) {
+                    firstClickTime = new Date().getTime() / 1000
+                    secondClickTime = null;
+                } else {
+                    secondClickTIme = new Date().getTime() / 1000;
+                    if ((secondClickTIme - firstClickTime) <= secondForDoubleClick) {
+                        firstClickTime = null;
+                        secondClickTime = null;
+                        var link = $(this);
+                        var url = link.attr("href");
+                        window.location.href = url;
+                    }
+                    firstClickTime = null;
+                    secondClickTime = null;
+                }
+            })
+        });
+        return this;
+    };
+}(jQuery));
